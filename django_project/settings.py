3c683f943f1810d6e8e057a958516ec0581fb677
Teresa Figuera <alejandromelrina324@gmail.com>
2024-02-07 10:09:09 -0400

    first commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pots.apps.PostConfig',
+    'posts.apps.PostConfig',
 ]
 
 MIDDLEWARE = [
